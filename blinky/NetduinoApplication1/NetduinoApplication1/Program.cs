bf960b1b5814d1fa7c14c1476b24c6bd80dbfbdc
sainathmcs <44135255+sainathmcs@users.noreply.github.com>
2019-10-06 13:57:47 +0530

    blinky

---
+++ blinky/NetduinoApplication1/NetduinoApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace NetduinoApplication1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Create an output port (a port that can be written to) 
+            // and wire it to the onboard LED
+            OutputPort led = new OutputPort(Pins.ONBOARD_LED, false);
+            // run forever
+            while (true)
+            {
+                led.Write(true); // turn on the LED
+                Thread.Sleep(250); // sleep for 250ms
+                led.Write(false); // turn off the LED
+                Thread.Sleep(250); // sleep for 250ms
+            }
+        }
+
+    }
+}
