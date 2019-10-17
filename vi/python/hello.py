57a2e80b2521b61ac5a56e049a0109b4c460a774
sainathmcs <44135255+sainathmcs@users.noreply.github.com>
2019-10-17 11:06:42 +0530

    added vi files

---
+++ vi/python/hello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 09:34:25 2019
+
+@author: saina
+"""
+
+def fun():
+    print("hello world!")
+    return 
+#fun()
---
+++ vi/python/temp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#a= ["sai","nath"]
+def fun(a):
+    print(a)
+    b=a*a
+    print(b)
+    return b
+fun(5)
